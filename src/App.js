08f9554e174b4f7029ff1f07d194570a67fa3d71
Vinicius Leme <vinileme@gmail.com>
2023-01-11 08:35:54 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <h1> Aqui Estamos </h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" component={ Profile } />
 
+          </Switch>
+          <Route path="*" component={ NotFound } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+
+        </BrowserRouter>
+
+      </div>
+    );
   }
 }
 
